19c8f04102f4172f4e89195f1e44039a053e88c3
Lueper <jkl410907@gmail.com>
2026-04-22 18:18:37 +0900

    Feat: IOCP, RIO 래핑 함수 추가

--- src/LpNetCore/LpSocketCore.cpp
+++ src/LpNetCore/LpSocketCore.cpp
@@ -2,44 +2,30 @@
 
 #include "LpSocketCore.h"
 
-SOCKET LpSocketCore::CreateIocpSocket() {
-	return ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
-}
-
-SOCKET LpSocketCore::CreateRioSocket() {
-	return ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED | WSA_FLAG_REGISTERED_IO);
-}
-
 bool LpSocketCore::Startup() {
 	WSADATA wsaData;
-	return SOCKET_SUCCESS == ::WSAStartup(MAKEWORD(2, 2), &wsaData);
+	return 0 == ::WSAStartup(MAKEWORD(2, 2), &wsaData);
 }
 
 bool LpSocketCore::Cleanup() {
 	return SOCKET_ERROR != ::WSACleanup();
 }
 
-bool LpSocketCore::Close(SOCKET& socket) {
-	if (socket == INVALID_SOCKET)
-		return false;
-
-	::closesocket(socket);
-	socket = INVALID_SOCKET;
-
-	return true;
+SOCKET LpSocketCore::CreateIocpSocket() {
+	return ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
 }
 
-bool LpSocketCore::Bind(SOCKET socket, unsigned short port) {
-	SOCKADDR_IN addr = {};
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	addr.sin_port = ::htons(port);
-
-	return SOCKET_ERROR != ::bind(socket, (const SOCKADDR*)&addr, sizeof(addr));
+HANDLE LpSocketCore::CreateIocpHandle() {
+	return ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 }
 
-bool LpSocketCore::Listen(SOCKET socket) {
-	return SOCKET_ERROR != ::listen(socket, SOMAXCONN);
+bool LpSocketCore::LoadExFunction(SOCKET socket, GUID guid, LPVOID* outFunc) {
+	DWORD bytes = 0;
+	return SOCKET_ERROR != ::WSAIoctl(socket, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), outFunc, sizeof(*outFunc), &bytes, NULL, NULL);
+}
+
+bool LpSocketCore::RegisterIocpHandle(SOCKET socket, HANDLE iocp, ULONG_PTR completionKey) {
+	return NULL != ::CreateIoCompletionPort((HANDLE)socket, iocp, completionKey, 0);
 }
 
 bool LpSocketCore::SetSockOpt(SOCKET socket, int level, int optName, const LPVOID optVal, int optLen) {
@@ -74,13 +60,71 @@ bool LpSocketCore::SetSendBufSize(SOCKET socket, int size) {
 	return SetSockOpt(socket, SOL_SOCKET, SO_SNDBUF, &size, sizeof(size));
 }
 
-bool LpSocketCore::LoadExFunction(SOCKET socket, GUID guid, LPVOID* outFunc) {
-	DWORD bytes = 0;
-	return SOCKET_ERROR != ::WSAIoctl(socket, SIO_GET_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), outFunc, sizeof(*outFunc), &bytes, NULL, NULL);
+bool LpSocketCore::Bind(SOCKET socket, unsigned short port) {
+	SOCKADDR_IN addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	addr.sin_port = ::htons(port);
+
+	return SOCKET_ERROR != ::bind(socket, (const SOCKADDR*)&addr, sizeof(addr));
 }
 
-bool LpSocketCore::LoadExFunctionTable(SOCKET socket, GUID guid, RIO_EXTENSION_FUNCTION_TABLE& outTable) {
+bool LpSocketCore::Listen(SOCKET socket) {
+	return SOCKET_ERROR != ::listen(socket, SOMAXCONN);
+}
+
+bool LpSocketCore::Close(SOCKET& socket) {
+	if (socket == INVALID_SOCKET)
+		return false;
+
+	::closesocket(socket);
+	socket = INVALID_SOCKET;
+
+	return true;
+}
+
+bool LpSocketCore::PopIocpEvent(HANDLE iocp, DWORD bytes, ULONG_PTR completionKey, OVERLAPPED* overlapped, DWORD timeoutMs) {
+	return ::GetQueuedCompletionStatus(iocp, &bytes, &completionKey, &overlapped, timeoutMs);
+}
+
+bool LpSocketCore::PushIocpEvent(HANDLE iocp, DWORD bytes, ULONG_PTR completionKey, OVERLAPPED* overlapped)
+{
+	return false;
+}
+
+std::string LpSocketCore::GetIpAddress(SOCKADDR_IN addr) {
+	char ipAddress[INET_ADDRSTRLEN] = { 0, };
+	::inet_ntop(AF_INET, &addr.sin_addr, ipAddress, sizeof(ipAddress));
+
+	return std::string(ipAddress);
+}
+
+SOCKET LpSocketCore::CreateRioSocket() {
+	return ::WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED | WSA_FLAG_REGISTERED_IO);
+}
+
+RIO_CQ LpSocketCore::CreateRioCompletionQueue(RIO_EXTENSION_FUNCTION_TABLE& rio, DWORD cqSize, RIO_NOTIFICATION_COMPLETION* notification) {
+	return rio.RIOCreateCompletionQueue(cqSize, notification);
+}
+
+RIO_RQ LpSocketCore::CreateRioRequestQueue(RIO_EXTENSION_FUNCTION_TABLE& rio, SOCKET socket, DWORD maxPendingRecv, DWORD maxRecvBuffers, DWORD maxPendingSend, DWORD maxSendBuffers, RIO_CQ recvCQ, RIO_CQ sendCQ, PVOID context) {
+	return rio.RIOCreateRequestQueue(socket, maxPendingRecv, maxRecvBuffers, maxPendingSend, maxSendBuffers, recvCQ, sendCQ, context);
+}
+
+bool LpSocketCore::LoadExFunctionTable(SOCKET socket, GUID guid, RIO_EXTENSION_FUNCTION_TABLE& rio) {
 	DWORD bytes = 0;
-	outTable.cbSize = sizeof(outTable);
-	return SOCKET_ERROR != ::WSAIoctl(socket, SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &outTable, sizeof(outTable), &bytes, NULL, NULL);
+	rio.cbSize = sizeof(rio);
+	return SOCKET_ERROR != ::WSAIoctl(socket, SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER, &guid, sizeof(guid), &rio, sizeof(rio), &bytes, NULL, NULL);
+}
+
+RIO_BUFFERID LpSocketCore::RegisterRioBuffer(RIO_EXTENSION_FUNCTION_TABLE& rio, char* buffer, DWORD size) {
+	return rio.RIORegisterBuffer(buffer, size);
+}
+
+ULONG LpSocketCore::PopRioEvent(RIO_EXTENSION_FUNCTION_TABLE& rio, RIO_CQ cq, RIORESULT* results, ULONG size) {
+	return rio.RIODequeueCompletion(cq, results, size);
+}
+
+bool LpSocketCore::NotifyRio(RIO_EXTENSION_FUNCTION_TABLE& rio, RIO_CQ cq) {
+	return 0 == rio.RIONotify(cq);
 }
--- src/LpNetCore/LpSocketCore.h
+++ src/LpNetCore/LpSocketCore.h
@@ -1,18 +1,18 @@
 ﻿#pragma once
 
-#define SOCKET_SUCCESS 0
+#include <string>
 
 class LpSocketCore {
 public:
-	static SOCKET CreateIocpSocket();
-	static SOCKET CreateRioSocket();
 	static bool Startup();
 	static bool Cleanup();
-	static bool Close(SOCKET& socket);
-	static bool Bind(SOCKET socket, unsigned short port);
-	static bool Listen(SOCKET socket);
 
-	// Set SockOpt
+	static SOCKET CreateIocpSocket();
+	static HANDLE CreateIocpHandle();
+
+	static bool LoadExFunction(SOCKET socket, GUID guid, LPVOID* outFunc);
+	static bool RegisterIocpHandle(SOCKET socket, HANDLE iocp, ULONG_PTR completionKey = 0);
+
 	static bool SetSockOpt(SOCKET socket, int level, int optName, const LPVOID optVal, int optLen);
 	static bool SetReuseAddr(SOCKET socket, BOOL optVal);
 	static bool SetNodelay(SOCKET socket, BOOL optVal);
@@ -21,9 +21,24 @@ public:
 	static bool SetRecvBufSize(SOCKET socket, int size);
 	static bool SetSendBufSize(SOCKET socket, int size);
 
-	static bool LoadExFunction(SOCKET socket, GUID guid, LPVOID* outFunc);
+	static bool Bind(SOCKET socket, unsigned short port);
+	static bool Listen(SOCKET socket);
+	static bool Close(SOCKET& socket);
+
+	static bool PopIocpEvent(HANDLE iocp, DWORD bytes, ULONG_PTR completionKey, OVERLAPPED* overlapped, DWORD timeoutMs = INFINITE);
+	static bool PushIocpEvent(HANDLE iocp, DWORD bytes = 0, ULONG_PTR completionKey = 0, OVERLAPPED* overlapped = nullptr);
+
+	static std::string GetIpAddress(SOCKADDR_IN addr);
 
 	// RIO
-	static bool LoadExFunctionTable(SOCKET socket, GUID guid, RIO_EXTENSION_FUNCTION_TABLE& outTable);
+	static SOCKET CreateRioSocket();
+	static RIO_CQ CreateRioCompletionQueue(RIO_EXTENSION_FUNCTION_TABLE& rio, DWORD cqSize, RIO_NOTIFICATION_COMPLETION* notification);
+	static RIO_RQ CreateRioRequestQueue(RIO_EXTENSION_FUNCTION_TABLE& rio, SOCKET socket, DWORD maxPendingRecv, DWORD maxRecvBuffers, DWORD maxPendingSend, DWORD maxSendBuffers, RIO_CQ recvCQ, RIO_CQ sendCQ, PVOID context);
+
+	static bool LoadExFunctionTable(SOCKET socket, GUID guid, RIO_EXTENSION_FUNCTION_TABLE& rio);
+	static RIO_BUFFERID RegisterRioBuffer(RIO_EXTENSION_FUNCTION_TABLE& rio, char* buffer, DWORD size);
+
+	static ULONG PopRioEvent(RIO_EXTENSION_FUNCTION_TABLE& rio, RIO_CQ cq, RIORESULT* results, ULONG size);
+	static bool NotifyRio(RIO_EXTENSION_FUNCTION_TABLE& rio, RIO_CQ cq);
 private:
 };
