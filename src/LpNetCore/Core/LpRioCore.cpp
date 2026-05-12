41f90998680a179e36c7f785264c0b5e8c1bbfc9
Lueper <jkl410907@gmail.com>
2026-05-12 19:13:35 +0900

    Feat: RIO Init() 구현

--- src/LpNetCore/Core/LpRioCore.cpp
+++ src/LpNetCore/Core/LpRioCore.cpp
@@ -27,9 +27,58 @@ LpRioCore::~LpRioCore() {
 }
 
 bool LpRioCore::Init() {
-	return false;
+	m_socket = CreateRioSocket();
+	if (m_socket == INVALID_SOCKET)
+		return false;
+
+	if (!LoadExFunction(m_socket, WSAID_ACCEPTEX, (LPVOID*)&AcceptEx))
+		return false;
+
+	if (!LoadExFunction(m_socket, WSAID_GETACCEPTEXSOCKADDRS, (LPVOID*)&GetAcceptExSockaddrs))
+		return false;
+
+	if (!LoadExFunctionTable(m_socket, WSAID_MULTIPLE_RIO, m_rio))
+		return false;
+
+	m_iocp = CreateIocpHandle();
+	if (m_iocp == NULL)
+		return false;
+
+	if (!RegisterIocpHandle(m_socket, m_iocp, CK_ACCEPT))
+		return false;
+
+	m_recvPool = (char*)VirtualAlloc(NULL, BUFFER_POOL_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	m_sendPool = (char*)VirtualAlloc(NULL, BUFFER_POOL_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (m_recvPool == nullptr || m_sendPool == nullptr)
+		return false;
+
+	m_recvBufferId = RegisterRioBuffer(m_rio, m_recvPool, BUFFER_POOL_SIZE);
+	m_sendBufferId = RegisterRioBuffer(m_rio, m_sendPool, BUFFER_POOL_SIZE);
+	if (m_recvBufferId == RIO_INVALID_BUFFERID || m_sendBufferId == RIO_INVALID_BUFFERID)
+		return false;
+
+	m_rioNotify.Type = RIO_IOCP_COMPLETION;
+	m_rioNotify.Iocp.IocpHandle = m_iocp;
+	m_rioNotify.Iocp.CompletionKey = (PVOID)CK_RIO;
+	m_rioNotify.Iocp.Overlapped = &m_overlapped;
+	m_rioCQ = CreateRioCompletionQueue(m_rio, CQ_SIZE, &m_rioNotify);
+	if (m_rioCQ == RIO_INVALID_CQ)
+		return false;
+
+	if (!SetReuseAddr(m_socket, true))
+		return false;
+
+	return true;
 }
 
 void LpRioCore::Start() {
+	if (!Bind(m_socket, SERVER_PORT))
+		return;
 
+	if (!Listen(m_socket))
+		return;
+
+	m_rio.RIONotify(m_rioCQ);
+
+	m_running = true;
 }
