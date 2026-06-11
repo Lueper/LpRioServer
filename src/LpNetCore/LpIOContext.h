bcd522af68cf6dccb6881aa6215cafec7cc06c92
Lueper <jkl410907@gmail.com>
2026-06-11 15:21:58 +0900

    Feat: OVERLAPPED 확장 LpIOContext 구현

---
+++ src/LpNetCore/LpIOContext.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "LpNetCore.h"
+
+struct LpIOContext : public OVERLAPPED {
+	LpIOContext(EIOType ioType) : m_ioType(ioType) {}
+
+	EIOType m_ioType;
+};
+
+struct LpAcceptContext : public LpIOContext {
+	LpAcceptContext() : LpIOContext(EIOType::Accept) {}
+};
+
+struct LpConnectContext : public LpIOContext {
+	LpConnectContext() : LpIOContext(EIOType::Connect) {}
+};
+
+struct LpDisconnectContext : public LpIOContext {
+	LpDisconnectContext() : LpIOContext(EIOType::Disconnect) {}
+};
+
+struct LpRecvContext : public LpIOContext {
+	LpRecvContext() : LpIOContext(EIOType::Recv) {}
+};
+
+struct LpSendContext : public LpIOContext {
+	LpSendContext() : LpIOContext(EIOType::Send) {}
+};
